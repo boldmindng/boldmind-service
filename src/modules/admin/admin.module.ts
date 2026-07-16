9e43cc8ae81a4077f5183d84670eaf47273a6b8f
charlesuchijuka <uchecharles223@gmail.com>
2026-07-17 00:09:07 +0100

    update the dependency issue

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,14 +1,15 @@
-
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AdminController } from './admin.controller';
-import { AdminService } from './admin.service';
-import { HealthController } from './health.controller';
-import { Post, PostSchema } from '../amebogist/schemas/post.schema';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AdminController } from "./admin.controller";
+import { AdminService } from "./admin.service";
+import { HealthController } from "./health.controller";
+import { Post, PostSchema } from "../amebogist/schemas/post.schema";
+import { WalletModule } from "../wallet/wallet.module";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
+    WalletModule,
   ],
   controllers: [AdminController, HealthController],
   providers: [AdminService],
--- src/modules/wallet/wallet.module.ts
+++ src/modules/wallet/wallet.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
-import { WalletController } from './wallet.controller';
-import { WalletService } from './wallet.service';
+import { Module } from "@nestjs/common";
+import { WalletController } from "./wallet.controller";
+import { WalletService } from "./wallet.service";
 
 /**
  * WalletModule
